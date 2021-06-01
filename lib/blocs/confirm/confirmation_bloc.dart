5b751f44010801c26a9073f2b2a08a8c176454bc
TanNHN <nhattan9101999@gmail.com>
2021-06-01 22:02:30 +0700

    LuanDP Update confirm 2

--- lib/blocs/confirm/confirmation_bloc.dart
+++ lib/blocs/confirm/confirmation_bloc.dart
@@ -1,6 +1,6 @@
-
 import 'package:car_service/blocs/confirm/confirmation_events.dart';
 import 'package:car_service/blocs/confirm/confirmation_state.dart';
+import 'package:car_service/form_submission_status.dart';
 import 'package:car_service/repository/auth_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -11,8 +11,18 @@ class ConfirmBloc extends Bloc<ConfirmEvents, ConfirmState> {
   @override
   Stream<ConfirmState> mapEventToState(ConfirmEvents event) async* {
     //Confirm code updated
-    if(event is ConfirmationCodeChanged) {
-      yield state.copy
+    if (event is ConfirmationCodeChanged) {
+      yield state.copyWith(code: event.code);
+
+      //Form submitted
+    } else if (event is ConfirmationSubmitted) {
+      yield state.copyWith(formStatus: FormSubmitting());
+      try {
+        // await repo.login(email, password)
+        yield state.copyWith(formStatus: SubmissionSuccess());
+      } catch (e) {
+        yield state.copyWith(formStatus: SubmissionFailed(e));
+      }
     }
   }
 }
